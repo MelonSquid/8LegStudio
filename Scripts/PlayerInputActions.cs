4bfab714500743c5c810f24210904aedad79d999
terceldrift <78244997+terceldrift@users.noreply.github.com>
2021-01-31 13:15:28 -0800

    Ready

--- Scripts/PlayerInputActions.cs
+++ Scripts/PlayerInputActions.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/8LegStudio/Scripts/PlayerInputActions.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/PlayerInputActions.inputactions'
 
 using System;
 using System.Collections;
